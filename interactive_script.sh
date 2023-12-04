a977708d5576538dda064b33c93d627bf001d3a3
pleiadev24 <rohinimrangan@gmail.com>
2023-12-04 12:18:24 +0530

    intearctive script

---
+++ interactive_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Welcome to the Interactive Script!"
+
+# Get user input
+read -p "Enter your name: " username
+
+# Display a personalized greeting
+echo "Hello, $username! This is your interactive script."
+
